5f285e937a9a09da9651ce8c2e417138b13f3703
msramalho <up201403027@fe.up.pt>
2018-10-12 00:17:03 +0100

    abstract Agent with more out-of-the-box functionality for other agents

---
+++ src/main/java/labyrinth/agents/AwareAgent.java
@@ -0,0 +1,48 @@
+package labyrinth.agents;
+
+import jade.lang.acl.ACLMessage;
+import labyrinth.maze.Maze;
+import labyrinth.maze.MazeKnowledge;
+import labyrinth.maze.MazePosition;
+import sajas.core.Agent;
+import sajas.core.behaviours.CyclicBehaviour;
+
+
+/**
+ * Maze Agent with some general useful skills, such as:
+ * Easy interface for storing and updating maze Knowledge confidences
+ * Easily receive and use messages
+ */
+public abstract class AwareAgent extends Agent {
+    protected MazePosition position;
+    protected MazeKnowledge knowledge;
+
+
+    public AwareAgent(MazePosition position, Maze maze) {
+        this.position = position;
+        knowledge = new MazeKnowledge(maze);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+        addBehaviour(new MessageBehaviour());
+    }
+
+    //TODO: not sure if this is worth isolating in another file (possibly not, as there may be more behaviours)
+    class MessageBehaviour extends CyclicBehaviour {
+        @Override
+        public void action() {
+            ACLMessage msg = receive();
+            if (msg != null) receiveMessage(msg);
+            block();// block doesn't stop execution - just schedules the next execution
+        }
+    }
+
+    /**
+     * Inheriting classes have a message to directly process received messages
+     *
+     * @param msg not null ACLMessage
+     */
+    protected abstract void receiveMessage(ACLMessage msg);
+}
