4aa69aae39e935dbae66a069f6f8af26da170a96
msramalho <up201403027@fe.up.pt>
2018-10-14 19:08:08 +0100

    fixed bug and simplified + toString for Direction

--- src/main/java/labyrinth/maze/Directions.java
+++ src/main/java/labyrinth/maze/Directions.java
@@ -5,8 +5,8 @@ import labyrinth.utils.Vector2D;
 import java.util.*;
 
 public enum Directions {
-    UP(new Vector2D(0, 1)),
-    DOWN(new Vector2D(0, -1)),
+    UP(new Vector2D(0, -1)),
+    DOWN(new Vector2D(0, 1)),
     LEFT(new Vector2D(-1, 0)),
     RIGHT(new Vector2D(1, 0));
 
@@ -16,24 +16,30 @@ public enum Directions {
         this.direction = direction;
     }
 
-    private static final List<Directions> directions = Arrays.asList(Directions.values());
+    public static final List<Directions> directions = Arrays.asList(Directions.values());
 
     public static List<Directions> getRandomDirections() {
         List<Directions> directionsCopy = new ArrayList<>(directions);
         Collections.shuffle(directionsCopy);
-            return directionsCopy;
+        return directionsCopy;
     }
 
     public static Directions getOpposite(Directions d1) {
-        if (d1 == UP)
-            return DOWN;
-        if (d1 == DOWN)
-            return UP;
-        if (d1 == LEFT)
-            return RIGHT;
-        if (d1 == RIGHT)
-            return LEFT;
+        if (d1 == UP) return DOWN;
+        if (d1 == DOWN) return UP;
+        if (d1 == LEFT) return RIGHT;
+        if (d1 == RIGHT) return LEFT;
         return null;
     }
 
+
+    //TODO: maybe remove after debug is done OR use reflection to simplify
+    @Override
+    public String toString() {
+        if (direction == UP.direction) return "UP";
+        if (direction == DOWN.direction) return "DOWN";
+        if (direction == RIGHT.direction) return "RIGHT";
+        if (direction == LEFT.direction) return "LEFT";
+        return "";
+    }
 }
