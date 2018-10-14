af8b0ee77b9e583cce32700a87c99e1888b60b98
msramalho <up201403027@fe.up.pt>
2018-10-14 19:09:31 +0100

    BacktrackAgent is working

---
+++ src/main/java/labyrinth/agents/BacktrackAgent.java
@@ -0,0 +1,46 @@
+package labyrinth.agents;
+
+import labyrinth.maze.Directions;
+import labyrinth.maze.Maze;
+import labyrinth.maze.MazePosition;
+import labyrinth.utils.Vector2D;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Stack;
+
+public class BacktrackAgent extends AwareAgent {
+
+    protected HashSet<Vector2D> seen = new HashSet<>();
+    protected Stack<Directions> backtrackStack = new Stack<>();
+
+    public BacktrackAgent(MazePosition position, Maze maze) { super(position, maze); }
+
+    @Override
+    public void tick() {
+        if (mazePosition.atExit()) return;
+        seen.add(mazePosition.getPosition()); // mark this as seen
+        mazePosition.move(getNextStep());
+    }
+
+    /**
+     * Implements a backtracking iteration: move to new cells or backtrack if no new is available
+     *
+     * @return the next step
+     */
+    private Directions getNextStep() {
+        ArrayList<Directions> directions = mazePosition.getAvailableDirections(true);
+
+        for (Directions d : directions)
+            if (!seen.contains(getPosAfterMove(d))) {
+                backtrackStack.push(Directions.getOpposite(d)); // add this to backtrackable steps
+                return d;
+            }
+
+        return backtrackStack.pop();// no new option -> go back
+    }
+
+    @Override
+    protected Color getAgentColor() {return Color.orange; }
+}
