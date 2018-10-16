8ff37686661db488a3bd18c009d75127c18b0eb3
ros <ros65536@gmail.com>
2018-10-16 17:36:27 +0100

    fix agents bug

--- src/main/java/labyrinth/LabyrinthModel.java
+++ src/main/java/labyrinth/LabyrinthModel.java
@@ -19,6 +19,7 @@ import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.gui.DisplaySurface;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
@@ -89,16 +90,18 @@ public class LabyrinthModel extends Repast3Launcher {
         // maze
         Maze maze = new MazeFactory(mazeSize).buildMaze();
 
+        List<Supplier<Vector2D>> agentPositions = new ArrayList<>();
         // agents
-        MazePosition mazePosition = new MazePosition(maze.startPos, maze);
+        MazePosition mazePosition1 = new MazePosition(maze.startPos, maze);
+        agentPositions.add(mazePosition1::getPosition);
+        AwareAgent agent = new ForwardAgent(mazePosition1, maze);
+        mainContainer.acceptNewAgent("dumb agent", agent).start();
 
-        // AwareAgent agent = new ForwardAgent(mazePosition, maze);
-        // mainContainer.acceptNewAgent("dumb agent", agent).start();
-
-        AwareAgent agent2 = new BacktrackAgent(mazePosition, maze);
+        MazePosition mazePosition2 = new MazePosition(maze.startPos, maze);
+        agentPositions.add(mazePosition2::getPosition);
+        AwareAgent agent2 = new BacktrackAgent(mazePosition2, maze);
         mainContainer.acceptNewAgent("backtrack agent", agent2).start();
 
-        List<Supplier<Vector2D>> agentPositions = Collections.singletonList(mazePosition::getPosition);
 
         // graphics
         new MazeSpace().addDisplayables(maze, agentPositions, displaySurf);
@@ -106,7 +109,7 @@ public class LabyrinthModel extends Repast3Launcher {
 
         // clock ticks
         ClockPublisher clockPublisher = new ClockPublisher();
-        // clockPublisher.subscribe(agent::tick);
+        clockPublisher.subscribe(agent::tick);
         clockPublisher.subscribe(agent2::tick);
         clockPublisher.subscribe(displaySurf::updateDisplay);
         getSchedule().scheduleActionAtInterval(actionRefreshRate, clockPublisher);
