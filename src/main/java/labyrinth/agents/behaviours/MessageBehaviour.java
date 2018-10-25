063774cd83ca1fe93b9f672e9f4098ae98b55d6b
msramalho <up201403027@fe.up.pt>
2018-10-25 12:59:18 +0100

    cleanup of message behaviour

--- src/main/java/labyrinth/agents/behaviours/MessageBehaviour.java
+++ src/main/java/labyrinth/agents/behaviours/MessageBehaviour.java
@@ -1,11 +1,9 @@
 package labyrinth.agents.behaviours;
 
-import jade.lang.acl.ACLMessage;
-import jade.lang.acl.UnreadableException;
 import labyrinth.agents.AwareAgent;
+import labyrinth.utils.ACLMessageC;
 import sajas.core.behaviours.Behaviour;
 
-import java.io.IOException;
 
 enum STATE {WAITING_CFP, WAITING_PROPOSAL, WAITING_ANSWER, DONE}
 
@@ -15,6 +13,7 @@ public class MessageBehaviour extends Behaviour {
 
     private STATE step = STATE.WAITING_CFP;
     private AwareAgent myAgent;
+    private long timeOfCFP;
 
     public MessageBehaviour(AwareAgent myAgent) {
         super(myAgent);
@@ -23,57 +22,64 @@ public class MessageBehaviour extends Behaviour {
 
     @Override
     public void action() {
-        ACLMessage msg = myAgent.receive();
+        ACLMessageC msg = (ACLMessageC) myAgent.receive();
         if (msg == null && step == STATE.WAITING_CFP) {
-            myAgent.send(myAgent.createCFP());
+            timeOfCFP = myAgent.sendTimestamp(myAgent.createCFP());
             return;
         } else if (msg == null) {
             block();
             return;
         }
 
+        myAgent.print(msg.getContentObject().toString() + " from " + msg.getSender().getName());
         //TODO: isolate to another function, state machine function
         switch (step) {
             case WAITING_CFP:
-                myAgent.print("step 0 in message behaviour");
-                if (msg.getPerformative() == ACLMessage.CFP) {
-                    try {
-                        String helloString = (String) msg.getContentObject();
-                        myAgent.print(helloString);
-                    } catch (UnreadableException e) {
-                        e.printStackTrace();
-                    }
-                    ACLMessage reply = msg.createReply();
-                    reply.setPerformative(ACLMessage.PROPOSE);
-                    try {
-                        reply.setContentObject("Tudo bem?");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    myAgent.send(reply);
+                if (msg.getPerformative() == ACLMessageC.CFP) {
+                    // If I sent my CFP after the other agent, it will ignore my CFP, and I will answer its
+                    if (timeOfCFP > msg.getPostTimeStamp() ||
+                            (timeOfCFP == msg.getPostTimeStamp() && 0 < myAgent.getName().compareTo(msg.getSender().getName()))) {
+                        myAgent.print("I was not the one to send CFP first");
+                        myAgent.sendTimestamp(myAgent.handleCFP(msg));
+                    } else myAgent.print("I was the first to send CFP");
+
                     step = STATE.WAITING_PROPOSAL;
+                    // try {
+                    //     String helloString = (String) msg.getContentObject();
+                    //     myAgent.print(helloString);
+                    // } catch (UnreadableException e) {
+                    //     e.printStackTrace();
+                    // }
+                    // ACLMessage reply = msg.createReply();
+                    // reply.setPerformative(ACLMessage.PROPOSE);
+                    // try {
+                    //     reply.setContentObject("Tudo bem?");
+                    // } catch (IOException e) {
+                    //     e.printStackTrace();
+                    // }
                 } else myAgent.print("RESET MACHINE 1!!");
 
                 break;
             case WAITING_PROPOSAL:
                 // myAgent.print("step 1 in message behaviour");
-                if (msg.getPerformative() == ACLMessage.PROPOSE) {
-                    try {
-                        String replyContent = (String) msg.getContentObject();
-                        if (replyContent.equals("Tudo bem?")) {
-                            ACLMessage accept = msg.createReply();
-                            accept.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
-                            accept.setContent("Sim, tudo!");
-                            myAgent.send(accept);
-                        } else {
-                            ACLMessage reject = msg.createReply();
-                            reject.setPerformative(ACLMessage.REJECT_PROPOSAL);
-                            reject.setContent("Não!");
-                            myAgent.send(reject);
-                        }
-                    } catch (UnreadableException e) {
-                        e.printStackTrace();
-                    }
+                if (msg.getPerformative() == ACLMessageC.PROPOSE) {
+                    myAgent.sendTimestamp(myAgent.handleProposal(msg));
+                    // try {
+                    //     String replyContent = (String) msg.getContentObject();
+                    //     if (replyContent.equals("Tudo bem?")) {
+                    //         ACLMessage accept = msg.createReply();
+                    //         accept.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+                    //         accept.setContent("Sim, tudo!");
+                    //         myAgent.sendTimestamp(accept);
+                    //     } else {
+                    //         ACLMessage reject = msg.createReply();
+                    //         reject.setPerformative(ACLMessage.REJECT_PROPOSAL);
+                    //         reject.setContent("Não!");
+                    //         myAgent.sendTimestamp(reject);
+                    //     }
+                    // } catch (UnreadableException e) {
+                    //     e.printStackTrace();
+                    // }
                 } else {
                     // myAgent.print("RESET MACHINE 2!!");
                     return;
@@ -81,19 +87,18 @@ public class MessageBehaviour extends Behaviour {
                 step = STATE.WAITING_ANSWER;
                 break;
             case WAITING_ANSWER:
-                myAgent.print("step 3 in message behaviour");
-                if (msg.getPerformative() == ACLMessage.ACCEPT_PROPOSAL) {
+                if (msg.getPerformative() == ACLMessageC.ACCEPT_PROPOSAL) {
+                    myAgent.sendTimestamp(myAgent.acceptedProposal(msg));
                     myAgent.print("Accepted!");
-                } else if (msg.getPerformative() == ACLMessage.REJECT_PROPOSAL) {
+                } else if (msg.getPerformative() == ACLMessageC.REJECT_PROPOSAL) {
+                    myAgent.rejectedProposal(msg);
                     myAgent.print("Rejected!");
-                } else {
-                    myAgent.print("RESET MACHINE 3!!");
-                    return;
-                }
+                } else return;
 
-                step= STATE.DONE;
+                step = STATE.DONE;
                 break;
         }
+        myAgent.print("In state: " + step.name());
     }
 
     @Override
