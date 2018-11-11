73d0ce2585c2e1b2fc84a50c1c35db67bbb7facc
msramalho <up201403027@fe.up.pt>
2018-11-11 13:07:39 +0000

    code cleanup

--- src/main/java/labyrinth/LabyrinthModel.java
+++ src/main/java/labyrinth/LabyrinthModel.java
@@ -2,7 +2,6 @@ package labyrinth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import jade.wrapper.StaleProxyException;
 import labyrinth.agents.AwareAgent;
 import labyrinth.cli.ConfigurationFactory;
 import jade.core.Profile;
@@ -111,12 +110,16 @@ public class LabyrinthModel extends Repast3Launcher {
 
         String extension = Utilities.getFileExtension(path);
         ObjectMapper yamlMapper;
-        if (extension.equals("json")) {
-            yamlMapper = new ObjectMapper();
-        } else if (extension.equals("yaml") || extension.equals("yml")) {
-            yamlMapper = new ObjectMapper(new YAMLFactory());
-        } else {
-            throw new IllegalArgumentException("invalid path: " + path);
+        switch (extension) {
+            case "json":
+                yamlMapper = new ObjectMapper();
+                break;
+            case "yaml":
+            case "yml":
+                yamlMapper = new ObjectMapper(new YAMLFactory());
+                break;
+            default:
+                throw new IllegalArgumentException("invalid path: " + path);
         }
 
         try {
