179a8837b0b43d25f24f792e81bc39fe377ed53d
msramalho <up201403027@fe.up.pt>
2018-11-27 18:34:50 +0000

    output fix

--- metrics/csv_merge.py
+++ metrics/csv_merge.py
@@ -1,12 +1,13 @@
 import os
 import pandas as pd
+import csv
+from tqdm import tqdm  # progress bar
 
 path_to_csv = 'data/'
 csv_files = ["%s%s" % (path_to_csv, pos_csv) for pos_csv in os.listdir(path_to_csv) if pos_csv.endswith('.csv')]
 
 print("Found %d csv files" % len(csv_files))
 
-from tqdm import tqdm  # progress bar
 pbar = tqdm(total=len(csv_files), mininterval=0.1, unit=" results")
 df = pd.read_csv(csv_files[0])
 for j in csv_files[1:]:
@@ -15,5 +16,5 @@ for j in csv_files[1:]:
 pbar.close()
 df.reset_index(inplace=True)
 
-df.to_csv("merged_all.csv",index=False)
+df.to_csv("merged_all.csv", index=False, quoting=csv.QUOTE_NONNUMERIC)
 print("Exported to csv")
